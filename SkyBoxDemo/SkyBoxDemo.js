7c8292bc8ec99e180356dc7804e8c266c5cc6bbd
Ryan Kim <ryankim0830@gmail.com>
2022-07-19 17:39:25 -0400

    Add project files.

---
+++ SkyBoxDemo/SkyBoxDemo.js
@@ -0,0 +1,109 @@
+﻿"use strict";
+
+var gl;
+
+var pointsArray = [];
+
+var va = vec4(0.0, 0.0, -1.0, 1);
+var vb = vec4(0.0, 0.942809, 0.333333, 1);
+var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
+var vd = vec4(0.816497, -0.471405, 0.333333, 1);
+
+var fovy = 37;
+var near = 0.1;
+var far = 100;
+var radius = 10;
+
+const at = vec3(0.0, 0.0, 0.0);
+var up = vec3(0.0, 1.0, 0.0);
+var eye = vec3(0, 0, radius);
+
+var modelViewMatrix;
+var modelViewMatrixLoc;
+
+window.onload = function init() {
+    var canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    // enable depth testing and polygon offset
+    // so lines will be in front of filled triangles
+
+    gl.enable(gl.DEPTH_TEST);
+
+    tetrahedron(va, vb, vc, vd, 5);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+
+    var vBufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+
+    var projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
+    var projectionMatrix = perspective(fovy, 1.0, near, far);
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+
+    render();
+}
+
+function tetrahedron(a, b, c, d, n) {
+    divideTriangle(a, b, c, n);
+    divideTriangle(d, c, b, n);
+    divideTriangle(a, d, b, n);
+    divideTriangle(a, c, d, n);
+}
+
+function triangle(a, b, c) {
+    pointsArray.push(a);
+    pointsArray.push(b);
+    pointsArray.push(c);
+}
+
+function divideTriangle(a, b, c, count) {
+    if (count > 0) {
+
+        var ab = mix(a, b, 0.5);
+        var ac = mix(a, c, 0.5);
+        var bc = mix(b, c, 0.5);
+
+        ab = normalize(ab, true);
+        ac = normalize(ac, true);
+        bc = normalize(bc, true);
+
+        divideTriangle(a, ab, ac, count - 1);
+        divideTriangle(ab, b, bc, count - 1);
+        divideTriangle(bc, c, ac, count - 1);
+        divideTriangle(ab, bc, ac, count - 1);
+    }
+    else {
+        triangle(a, b, c);
+    }
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    modelViewMatrix = lookAt(eye, at, up);
+
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
+    gl.drawArrays(gl.TRIANGLES, 0, pointsArray.length);
+
+    requestAnimFrame(render);
+}
